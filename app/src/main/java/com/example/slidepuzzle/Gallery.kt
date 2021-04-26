e8468c626ce636fb6cac70cee431b3e8966a0acb
Emma Garcia <egarciasanti@uoc.edu>
2021-04-26 21:53:35 +0200

    La galeria funciona locooooo

--- app/src/main/java/com/example/slidepuzzle/Gallery.kt
+++ app/src/main/java/com/example/slidepuzzle/Gallery.kt
@@ -1,11 +1,44 @@
 package com.example.slidepuzzle
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.ImageView
+import com.example.slidepuzzle.ui.boardoptions.BoardTitledSize
+
 
 class Gallery : AppCompatActivity() {
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    private val pickImage = 100
+    private var imageUri: Uri? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.galeria)
+        title = "KotlinApp"
+        imageView = findViewById(R.id.imageView)
+        button = findViewById(R.id.buttonLoadPicture)
+        button.setOnClickListener {
+            val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            startActivityForResult(gallery, pickImage)
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == pickImage) {
+            imageUri = data?.data
+            imageView.setImageURI(imageUri)
+            GameActivity.initialConfig = BoardActivityParams(
+                MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri),
+                BoardTitledSize(3,3)
+
+            )
+            startActivity(
+                Intent(this, GameActivity::class.java)
+            )
+        }
     }
 }
